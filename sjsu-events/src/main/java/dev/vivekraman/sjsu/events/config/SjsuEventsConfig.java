8fd3789861054f5f9ec3b79917c4273383dc15ef
Vivek Raman <vr.ac4bf@live.com>
2025-04-29 14:11:18 -0700

    Integrate Localist SDK

--- sjsu-events/src/main/java/dev/vivekraman/sjsu/events/config/SjsuEventsConfig.java
+++ sjsu-events/src/main/java/dev/vivekraman/sjsu/events/config/SjsuEventsConfig.java
@@ -1,8 +1,16 @@
 package dev.vivekraman.sjsu.events.config;
 
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import dev.vivekraman.localist.sdk.LocalistAPI;
+import dev.vivekraman.localist.sdk.internal.LocalistAPIClient;
+import dev.vivekraman.localist.sdk.internal.LocalistProperties;
 
 @Configuration
 public class SjsuEventsConfig {
@@ -10,7 +18,20 @@ public class SjsuEventsConfig {
   public GroupedOpenApi sjsuEventsApiGroup() {
     return GroupedOpenApi.builder()
         .group(Constants.MODULE_NAME)
-        .packagesToScan("dev.vivekraman.module.controller")
+        .packagesToScan("dev.vivekraman.sjsu.events.controller")
         .build();
+  }
+
+  @Bean
+  public LocalistAPI localistAPI(
+      @Value("${PORT:8080}") String port, LocalistProperties props) {
+    WebClient.Builder builder = WebClient.builder()
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .defaultHeader(HttpHeaders.ORIGIN, "http://localhost:" + port)
+        .defaultHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        .defaultHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS")
+        .defaultHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*")
+        .defaultHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+    return new LocalistAPIClient(builder, props);
   }
 }
--- sjsu-events/src/main/java/dev/vivekraman/sjsu/events/controller/TestController.java
+++ sjsu-events/src/main/java/dev/vivekraman/sjsu/events/controller/TestController.java
@@ -1,21 +1,44 @@
 package dev.vivekraman.sjsu.events.controller;
 
 import dev.vivekraman.sjsu.events.config.Constants;
+import dev.vivekraman.localist.sdk.LocalistAPI;
+import dev.vivekraman.localist.sdk.model.Event;
 import dev.vivekraman.monolith.annotation.MonolithController;
 import dev.vivekraman.monolith.model.Response;
+import dev.vivekraman.monolith.model.ResponseList;
 import lombok.RequiredArgsConstructor;
+
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 
 @MonolithController(moduleName = Constants.MODULE_NAME)
 @RequiredArgsConstructor
 public class TestController {
+  private final LocalistAPI localistAPI;
   private final Scheduler scheduler;
 
   @GetMapping
-  public Mono<Response<Boolean>> test() {
-    return Mono.just(Response.of(true))
-        .subscribeOn(scheduler);
+  public Mono<ResponseList<Event>> fetchEvents(
+      @RequestParam(required = false, defaultValue = "0") int page,
+      @RequestParam(required = false, defaultValue = "10") int size) {
+    ZonedDateTime start = ZonedDateTime.now();
+    ZonedDateTime end = start.plus(1, ChronoUnit.DAYS);
+    return localistAPI.getEvents(start, end, page, size)
+        .collectList()
+        .map((events) -> {
+          ResponseList<Event> response = ResponseList.of(events);
+          response.setData(events);
+          response.setPage(null); // TODO: set page info
+          response.setSize(null);
+          response.setTotal(null);
+          return response;
+        }).subscribeOn(scheduler);
   }
 }
